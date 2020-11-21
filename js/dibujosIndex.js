2dfc9ae82be1463129592587c4ebd92159343665
Jose David Castañeda <51899338+david12997@users.noreply.github.com>
2020-11-21 18:06:13 -0500

    se terminaron las paginas catalogo e inicio

---
+++ js/dibujosIndex.js
@@ -0,0 +1,243 @@
+$(window).on('load',function () {
+
+ 
+
+    
+        const  Descuentos=(data,size)=>{
+          //console.log(size)
+
+     
+              
+            let banner= `
+
+
+            <svg class="svg-principal" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="${size}">
+            <defs>
+              <style>
+                .cls-1, .cls-11, .cls-12, .cls-13 {
+                  fill: none;
+                }
+          
+                .cls-2 {
+                  isolation: isolate;
+                }
+          
+                .cls-15, .cls-3 {
+                  fill: #fff;
+                }
+          
+                .cls-4 {
+                  clip-path: url(#clip-path);
+                }
+          
+                .cls-5 {
+                  opacity: 0.79;
+                }
+          
+                .cls-6 {
+                  opacity: 0.75;
+                }
+          
+                .cls-10, .cls-6, .cls-8 {
+                  mix-blend-mode: multiply;
+                }
+          
+                .cls-17, .cls-7, .cls-8 {
+                  fill: #212121;
+                }
+          
+                .cls-9 {
+                  fill: #2caf35;
+                }
+          
+                .cls-10 {
+                  fill: #d32f2f;
+                  opacity: 0.54;
+                }
+          
+                .cls-11, .cls-13 {
+                  stroke: #212121;
+                }
+          
+                .cls-11, .cls-12, .cls-13 {
+                  stroke-miterlimit: 10;
+                  stroke-width: 1.5px;
+                }
+          
+                .cls-11 {
+                  mix-blend-mode: overlay;
+                }
+          
+                .cls-12 {
+                  stroke: #fff;
+                  mix-blend-mode: screen;
+                }
+          
+                .cls-14 {
+                  fill: #f9f7f7;
+                }
+          
+                .cls-15 {
+                  font-size: 80.26px;
+                  font-family: Antonio-Bold, Antonio Bold;
+                  font-weight: 700;
+                }
+          
+                .cls-16 {
+                  letter-spacing: -0.07em;
+                }
+          
+                .cls-17 {
+                  font-size: 65.08px;
+                }
+          
+                .cls-17, .cls-21 {
+                  font-family: Yellowtail;
+                }
+          
+                .cls-18 {
+                  letter-spacing: -0.01em;
+                }
+          
+                .cls-19 {
+                  letter-spacing: 0em;
+                }
+          
+                .cls-20 {
+                  letter-spacing: 0em;
+                }
+          
+                .cls-21 {
+                  font-size: 41px;
+                }
+          
+                .cls-21, .cls-23 {
+                  fill: #fff8f8;
+                }
+          
+                .cls-22 {
+                  letter-spacing: -0.03em;
+                }
+
+                .cls-23{
+                  font-size: 20px;
+                  fill: #fff8f8;
+
+                }
+              </style>
+              <clipPath id="clip-path">
+                <rect class="cls-1" width="1361.96" height="449"/>
+              </clipPath>
+            </defs>
+            <g class="cls-2">
+              <g id="OBJECTS">
+                <g>
+                  <g>
+                    <rect class="cls-3" width="1361.96" height="449"/>
+                    <g class="cls-4">
+                      <g>
+                        <g>
+                          <image class="cls-6" width="840" height="1161" transform="translate(-14.12 -491.7)" xlink:href="assets/svg/banner-new1.png"/>
+                          <path class="cls-7" d="M268.81,606,8.9,73.45A70.8,70.8,0,0,1,19.64-4.71L414.13-451.87c34.95-39.62,100.15-31.11,123.18,16.08L797.22,96.76a70.84,70.84,0,0,1-10.73,78.17L392,622.09C357,661.7,291.84,653.19,268.81,606Z"/>
+                        </g>
+                        <path class="cls-8" d="M229.17,427.33-30.74-105.23A70.84,70.84,0,0,1-20-183.39L374.49-630.55c34.95-39.62,100.16-31.11,123.18,16.08L757.58-81.92A70.84,70.84,0,0,1,746.85-3.75L352.36,443.4C317.4,483,252.2,474.51,229.17,427.33Z"/>
+                        <g>
+                          <image class="cls-6" width="828" height="1150" transform="translate(-166.12 -353.7)" xlink:href="assets/svg/banner-new2.png"/>
+                          <path class="cls-9" d="M110.29,739.16-149.62,206.6a70.86,70.86,0,0,1,10.74-78.17L255.61-318.72c34.95-39.62,100.16-31.11,123.19,16.07L638.71,229.91A70.86,70.86,0,0,1,628,308.08L233.48,755.23C198.53,794.85,133.32,786.34,110.29,739.16Z"/>
+                        </g>
+                        <path class="cls-10" d="M-156.1,297-416-235.53a70.84,70.84,0,0,1,10.73-78.17l394.5-447.16c35-39.61,100.15-31.1,123.18,16.08L372.31-212.22a70.8,70.8,0,0,1-10.74,78.16L-32.92,313.1C-67.87,352.72-133.07,344.21-156.1,297Z"/>
+                        <path class="cls-11" d="M32.59,628.26-227.32,95.71a70.83,70.83,0,0,1,10.73-78.17L177.9-429.62c35-39.62,100.16-31.11,123.19,16.08L561,119a70.86,70.86,0,0,1-10.74,78.17L155.77,644.34C120.82,684,55.61,675.45,32.59,628.26Z"/>
+                        <path class="cls-12" d="M350.1-301.54c34.95-39.62,100.15-31.11,123.18,16.07L733.19,247.09a70.82,70.82,0,0,1-10.74,78.17L328,772.41C293,812,227.81,803.52,204.78,756.34"/>
+                      </g>
+                    </g>
+                  </g>
+                  <g>
+                    <rect class="cls-13" x="52.47" y="348.06" width="194.02" height="43.38" rx="10"/>
+                    <ellipse class="cls-7" cx="411.48" cy="336.02" rx="92.52" ry="82.08"/>
+                  </g>
+                </g>
+                <path class="cls-14" d="M454.72,350.17H393.06l1.37,6.73h56.46a5.06,5.06,0,0,1,4.93,6.17l-1.17,5.1a11.79,11.79,0,1,1-13.37,2.19h-44.1a11.78,11.78,0,1,1-14.1-1.8L368.3,296.32H353.6a5,5,0,0,1-5.05-5.05v-3.36a5,5,0,0,1,5.05-5.05h21.57a5,5,0,0,1,4.94,4l1.93,9.43h82.62a5,5,0,0,1,4.92,6.17l-9.94,43.75A5,5,0,0,1,454.72,350.17Zm-20.35-32H424.28v-8.42a3.36,3.36,0,0,0-3.37-3.36h-3.36a3.36,3.36,0,0,0-3.37,3.36v8.42h-10.1a3.36,3.36,0,0,0-3.36,3.36v3.37a3.36,3.36,0,0,0,3.36,3.36h10.1v8.42a3.36,3.36,0,0,0,3.37,3.36h3.36a3.36,3.36,0,0,0,3.37-3.36v-8.42h10.09a3.36,3.36,0,0,0,3.37-3.36v-3.37A3.36,3.36,0,0,0,434.37,318.2Z"/>
+              </g>
+              <g id="titulo-mas-grande">
+                <text class="cls-15" transform="translate(18.11 208.41) scale(1.03 1)">${data[0].descuento}</text>
+              </g>
+              <g id="titulo-mas-grande">
+                <text class="cls-23" transform="translate(18.11 30.41) scale(1.03 1)">${data[0].condiciones}</text>
+              </g>
+              <g id="comprar">
+                <text class="cls-17" transform="matrix(1.03, -0.03, -0.09, 1, 35.09, 295.28)">${data[0].textComprar}</text>
+              </g>
+              <g id="boton-ver-mas">
+                <text class="cls-21" transform="translate(80.07 380.6) scale(1.03 1)">${data[0].btn}</text>
+              </g>
+              <g id="img4">
+                <image width="209" height="932" transform="translate(889.88 23.35) scale(0.21)" xlink:href="${data[0].imagen1}"/>
+              </g>
+              <g id="img3">
+                <image width="672" height="734" transform="translate(1002.81 14.51) scale(0.29)" xlink:href="${data[0].imagen2}"/>
+              </g>
+              <g id="img-2">
+                <image width="857" height="455" transform="matrix(0.43, 0.04, -0.04, 0.43, 829.44, 215.78)" xlink:href="${data[0].imagen3}"/>
+              </g>
+              <g id="img-1">
+                <image width="765" height="540" transform="matrix(0, -0.47, 0.47, 0, 1250.55, 406.63)" xlink:href="${data[0].imagen4}"/>
+              </g>
+            </g>
+          </svg>
+          
+            
+            
+            `;
+
+            return banner;
+
+        }
+
+        fetch('data/banner-index.json')
+        .then(res=>res.json())
+        .then(data=>{
+          let draw;
+          let tamaño_pantalla;
+          let size_banner;
+
+          const PintarDescuento=()=>{
+
+            tamaño_pantalla= $(window).width();
+            //console.log(tamaño_pantalla);
+
+            if(tamaño_pantalla>=1000){
+              size_banner="0 0 1505.4 445.86";
+            }else{
+              size_banner="0 0 1200.4 445.86 ";
+            }
+            draw= Descuentos(data,size_banner)
+            $('.svg-principal').remove();
+            $('.cont-descuento').append(draw);
+          }
+   
+          
+         
+
+            setInterval(PintarDescuento,1000)
+     
+        });
+
+        
+})
+
+
+/* imagenes.addEventListener('animationstart',(e)=>{
+
+                              //contador para cammbiar imagen
+                              if(i==data.length-1){
+                                i=0;
+                              }else{
+                                i++;
+                              }
+             
+                              //cambiar imagen
+                              $('.mi-imagen')[0].href.baseVal=data[i].imagen1
+                              $('.mi-imagen')[1].href.baseVal=data[i].imagen2
+
+
+            }); */
